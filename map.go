b085b9a495a8614645129b7b6348003e8c6d0926
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-25 00:36:19 -0300

    first map

---
+++ map.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	mapa := map[string][]string{
+		"carlos_antonio": {"video game", "read", "program"},
+		"boarro_lara":    {"play with dogs", "program", "drink"},
+		"luiz_eduardo":   {"nargs", "drink", "play cards"},
+	}
+
+	for key, value := range mapa {
+		fmt.Print(key, " like: ")
+		for _, val := range value {
+			fmt.Print(val, " ")
+		}
+		fmt.Print("\n")
+	}
+}
